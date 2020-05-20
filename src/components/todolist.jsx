476f0372a3c9f28a2333168c232464c322fea111
sy0837 <50517233+sy0837@users.noreply.github.com>
2020-05-20 14:56:06 +0530

    Layout updated

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -13,19 +13,31 @@ import {
 
 const useStyle = makeStyles(theme => ({
     root: {
-        padding: theme.spacing(2),
+        
         // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderBottom : 'solid 1px #000',
+        paddingTop : '16px',
+        paddingLeft : '20px',
+        paddingRight : '20px',
+        paddingBottom : '16px'
         
     },
     main: {
         marginTop: theme.spacing(1),
     },
     border: {
-        padding: '8px'
+        padding: '2px'
     },
     button :{
         background: '#FE6B8B',
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        marginTop : '0'
+    },
+    // paper :{
+    //     boxShadow: ' 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+    // }
+    typo :{
+        marginTop : '10px'
     }
 }))
 
@@ -33,15 +45,15 @@ export default (props) => {
     const classes = useStyle()
     return (
         <div className={classes.main}>
-            <Paper variant="outlined" >
+            <Paper variant="outlined" className={classes.paper} >
                 {props.todos.map(todo => {
                     if (props.listId === todo.list_id) {
                         return (
                             <div key={todo._id} className={classes.root} >
                                 <Grid container spacing={2}>
-                                    <Grid item xs={10}>
+                                    <Grid item xs={11}>
                                         {
-                                            <Typography>
+                                            <Typography className={classes.typo}>
                                                 {todo.name}
                                             </Typography>}
                                     </Grid>
